3e6f3f44b884fe7c2a10e3075d1a7a62eb1115fb
Martin Beaumont <37398960+martinbmnt@users.noreply.github.com>
2025-03-05 14:38:07 +0100

    Demo - Ajout du plugin de démo

---
+++ demo/plugin/wpinteractivitydemo-plugin.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name:       WP Interactivity Demo Plugin
+ * Description:       Example blocks for demonstration purpose at WP MeetUp.
+ * Version:           0.1.0
+ * Requires at least: 6.7
+ * Requires PHP:      7.4
+ * Author:            Martin Beaumont
+ * Author URI:        https://www.martinbeaumont.dev/
+ * Text Domain:       wpinteractivitydemo-plugin
+ * Domain Path:       /languages
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * @package WP_Interactivity_Demo
+ * @see https://developer.wordpress.org/news/2024/09/how-to-build-a-multi-block-plugin/
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
